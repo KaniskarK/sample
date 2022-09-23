554f83f394593f3a8b0c1fe34e7d8d609e3cb4c1
KaniskarK <kaniskark@outlook.com>
2022-09-23 22:18:05 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
 const App = () => {
     return React.createElement("div", {}, [
       React.createElement("h1", {}, "Adopt Me!"),
